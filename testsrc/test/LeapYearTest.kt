fcfccbbe7b95b373470847524e596b2d82ea9cfe
kuwalab <arata.shinichi@gmail.com>
2015-03-07 17:43:50 +0900

    余分な空行の除去

--- testsrc/test/LeapYearTest.kt
+++ testsrc/test/LeapYearTest.kt
@@ -15,4 +15,20 @@ class LeapYearTest {
     Test fun 西暦1584年は閏年() {
         assertEquals(true, isLeapYear(1584))
     }
+
+    Test fun 西暦1900年は閏年ではない() {
+        assertEquals(true, isLeapYear(2004))
+    }
+
+    Test fun 西暦2000年は閏年() {
+        assertEquals(true, isLeapYear(2000))
+    }
+
+    Test fun 西暦2004年は閏年() {
+        assertEquals(true, isLeapYear(2004))
+    }
+
+    Test fun 西暦2005年は閏年ではない() {
+        assertEquals(false, isLeapYear(2005))
+    }
 }
