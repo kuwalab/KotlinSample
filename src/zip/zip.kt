632eb974476838c128abb1e0331822bb17d9afc1
kuwalab <arata.shinichi@gmail.com>
2015-03-13 09:11:12 +0900

    zipファイルの展開サンプル追加

---
+++ src/zip/zip.kt
@@ -0,0 +1,31 @@
+package zip
+
+import java.io.FileInputStream
+import java.util.zip.ZipInputStream
+import java.nio.file.Paths
+import java.nio.file.Files
+import java.io.FileOutputStream
+
+fun main(args: Array<String>) {
+    ZipInputStream(FileInputStream(Paths.get("res/test.zip").toFile())).use { (zis) ->
+        while (true) {
+            var entry = zis.getNextEntry()
+            if (entry == null) break
+
+            println(entry.getName())
+            if (entry.isDirectory()) {
+                Files.createDirectories(Paths.get("res", "zip", entry.getName()))
+            } else {
+                FileOutputStream(Paths.get("res", "zip", entry.getName()).toFile()).use { (fos) ->
+                    var buf = ByteArray(1024 * 8)
+                    var length = 0
+                    while (true) {
+                        length = zis.read(buf)
+                        if (length == -1) break;
+                        fos.write(buf, 0, length)
+                    }
+                }
+            }
+        }
+    }
+}
