dbeb34c0864ef76b742e6631b84dca0cec3ed8e9
kuwalab <arata.shinichi@gmail.com>
2015-04-04 05:41:26 +0900

    try-with-resourcesのサンプルの追加

---
+++ src/tips/TryWithResources.kt
@@ -0,0 +1,18 @@
+package tips.t001
+
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun main(args: Array<String>) {
+    var outputPath = Paths.get("outputText.txt")
+    try {
+        Files.newBufferedWriter(outputPath, StandardCharsets.UTF_8).use {
+            it.append("あいうえお")
+            it.newLine()
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+}
