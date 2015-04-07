9a31a4a87fcb7ba3dcb949bb892dc2d2b6e26e62
kuwalab <arata.shinichi@gmail.com>
2015-04-07 18:31:20 +0900

    ファイルコピーのサンプル追加

--- src/tips/CopyFIle.kt
+++ src/tips/CopyFIle.kt
@@ -1,15 +1,23 @@
 package tips.t002
 
+import java.io.File
 import java.nio.file.Files
 import java.nio.file.Paths
 
 fun main(args: Array<String>) {
-    val srcFile = Paths.get("res/test.zip")
-    val destFile = Paths.get("res/dest.zip")
-    Files.newInputStream(srcFile).use { ins ->
-        Files.newOutputStream(destFile).use { os ->
-            val size = ins.copyTo(os, bufferSize = 1024 * 8)
-            println("ファイルサイズ=${size}")
+    // Fileのコピー
+    val srcFile = File("res/test.zip")
+    val destFile = File("res/dest1.zip")
+    val size1 = srcFile.copyTo(destFile, bufferSize = 1024 * 8)
+    println("ファイルサイズ=${size1}")
+
+    // InputStreamのコピー
+    val srcPath = Paths.get("res/test.zip")
+    val destPath = Paths.get("res/dest2.zip")
+    Files.newInputStream(srcPath).use { ins ->
+        Files.newOutputStream(destPath).use { os ->
+            val size2 = ins.copyTo(os, bufferSize = 1024 * 8)
+            println("ファイルサイズ=${size2}")
         }
     }
 }
